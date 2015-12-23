09f687bd8e5ebae18475d70dd974bc8df0ebccc4
mjlyons <buzz.github@clearhive.com>
2015-12-23 16:01:41 -0500

    cleanup - submodularize z

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # dotfiles
 Contains my dot files (.vimrc, .bashrc, etc.)
 
+## Usage
+
+* Install Inconsolas font
+* Set up symlinks:
+  `ln -s $REPO/.vimrc ~/`
+  `ln -s $REPO/.vim ~/`
+  `ln -s REPO/.bashrc ~/.bash_profile`  (for OS X)
+
--- source-me.sh
+++ source-me.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+pushd "$(pwd)"
+
 # Switch working folder to the one containing this script
-script_dir="${BASH_SOURCE%/*}"
+scriptDir="${BASH_SOURCE%/*}"
+
 cd "$script_dir"
 
 echo "- sourcing z"
-source z/z.sh
+cd "$scriptDir" && source z/z.sh
 
 echo "- sourcing git completion"
-source git/git-completion.bash
+source git-completion/git-completion.bash
+
+popd
 
